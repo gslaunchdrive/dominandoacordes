f0da0a03c91301012b7b15342e1add86f07151f2
VGS <gs.launchdrive@gmail.com>
2023-10-15 22:16:54 -0300

    videos autoplay

--- perpetuo/js/main.js
+++ perpetuo/js/main.js
@@ -4,8 +4,8 @@ $(document).ready(function () {
     $('#videoVendas').click(function() {
         $('#loadingOverlay').css('display', 'flex');
     
-        $(this).replaceWith('<iframe width="560" height="600" src="https://www.youtube.com/embed/jfKfPfyJRdk" \
-             title="Mestres da Harmonia" frameborder="0" \
+        $(this).replaceWith('<iframe width="560" height="600" src="https://www.youtube.com/embed/jfKfPfyJRdk?autoplay=1" \
+             title="Video de Vendas" frameborder="0" \
              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" \
              allowfullscreen></iframe>');
     
@@ -17,7 +17,7 @@ $(document).ready(function () {
       $('#videoReharmonizacao').click(function() {
         $('#loadingOverlay1').css('display', 'flex');
     
-        $(this).replaceWith('<iframe width="450" height="300" src="https://www.youtube.com/embed/BTbwJqTBSeA" \
+        $(this).replaceWith('<iframe width="450" height="300" src="https://www.youtube.com/embed/BTbwJqTBSeA?autoplay=1" \
              title="Rearmonizacao - Gilvanio" frameborder="0" \
              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" \
              allowfullscreen></iframe>');
@@ -30,7 +30,7 @@ $(document).ready(function () {
       $('#videoDepoimentoPedro').click(function() {
         $('#loadingOverlay2').css('display', 'flex');
     
-        $(this).replaceWith('<iframe width="100%" height="350" src="https://www.youtube.com/embed/7KEU8kJpsao" \
+        $(this).replaceWith('<iframe width="100%" height="350" src="https://www.youtube.com/embed/7KEU8kJpsao?autoplay=1" \
              title="Depoimento - Pedro" frameborder="0" \
              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" \
              allowfullscreen></iframe>');
@@ -43,7 +43,7 @@ $(document).ready(function () {
       $('#videoDepoimentoPaulo').click(function() {
         $('#loadingOverlay3').css('display', 'flex');
     
-        $(this).replaceWith('<iframe width="100%" height="350" src="https://www.youtube.com/embed/pqc-BX2wl4M" \
+        $(this).replaceWith('<iframe width="100%" height="350" src="https://www.youtube.com/embed/pqc-BX2wl4M?autoplay=1" \
              title="Depoimento - Paulo" frameborder="0" \
              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" \
              allowfullscreen></iframe>');
@@ -56,7 +56,7 @@ $(document).ready(function () {
       $('#videoDepoimentoJoao').click(function() {
         $('#loadingOverlay4').css('display', 'flex');
     
-        $(this).replaceWith('<iframe width="100%" height="350" src="https://www.youtube.com/embed/Ych8kIbCuaE" \
+        $(this).replaceWith('<iframe width="100%" height="350" src="https://www.youtube.com/embed/Ych8kIbCuaE?autoplay=1" \
              title="Mestres da Harmonia" frameborder="0" \
              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" \
              allowfullscreen></iframe>');
@@ -69,7 +69,7 @@ $(document).ready(function () {
       $('#videoDepoimentoLuan').click(function() {
         $('#loadingOverlay5').css('display', 'flex');
     
-        $(this).replaceWith('<iframe width="100%" height="350" src="https://www.youtube.com/embed/LIoN2WLpl7Q" \
+        $(this).replaceWith('<iframe width="100%" height="350" src="https://www.youtube.com/embed/LIoN2WLpl7Q?autoplay=1" \
              title="Mestres da Harmonia" frameborder="0" \
              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" \
              allowfullscreen></iframe>');
